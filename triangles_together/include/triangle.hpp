77b0edacfbae6aae0950dfa0cb860475d1903d7e
ExactlyWb <theprojectexactly@gmail.com>
2021-10-17 20:00:30 +0300

    Function for intersection 3D triangles not done yet.

--- triangles_together/include/triangle.hpp
+++ triangles_together/include/triangle.hpp
@@ -105,7 +105,7 @@ namespace GObjects {
 		
 		}
 
-		void CalcNormal (Vector<pType> &normalVector) {
+		void calcNormal (Vector<pType> &normalVector) const {
 
 			Vector<pType> firstSide 	= rVecs_ [0] - rVecs_ [1];
 			Vector<pType> secondSide 	= rVecs_ [0] - rVecs_ [2];
@@ -116,14 +116,14 @@ namespace GObjects {
 
 		}
 
-		void calcCoefD (Vector<pType> &normalV, pType &ourCoefD) {
+		void calcCoefD (Vector<pType> &normalV, pType &ourCoefD) const {
 
 			ourCoefD = 0;
 
 			for (int i = 0; i < 3; ++i) {
 
 				ourCoefD += normalV.getCoord (i) * rVecs_ [0].getCoord (i);
-				std::cout << ourCoefD << std::endl;
+				//std::cout << ourCoefD << std::endl;
 
 			}
 
@@ -164,7 +164,7 @@ namespace GObjects {
 			//fall asleep
 
 			// return intersectTwoTriangle (...);
-        }
+        }    
 
     };
         //##############################################################################
@@ -189,6 +189,122 @@ namespace GObjects {
         std::ostream &operator << (std::ostream &out, const Triangle<pType> &triangle) {
             out << "{ " << triangle.getVec(0) << " ;\n " << triangle.getVec(1) << " ;\n " << triangle.getVec(2) << " }\n";
             return out;
+        }
+
+        
+        //##############################################################################
+        //                         TRIANGLE-3D INTERSECTION (Frolov)
+        //##############################################################################
+         
+        template<typename pType>
+        void CountCommonP  (pType firstD, pType secondD, Vector<pType>& firstNormalVec, 
+                            Vector<pType>& secondNormalVec, Vector<pType>& commonP) {
+        
+            //here we calc pointer(P) on common lane
+            //\vec{P} = a \cdot \vec{n_1} + b \cdot \vec{n_2}
+            pType nScalarProduct    = firstNormalVec * secondNormalVec;
+            pType n1SqLength        = firstNormalVec.squareLength ();
+            pType n2SqLength        = secondNormalVec.squareLength ();
+
+            pType commonDenominator = nScalarProduct * nScalarProduct - n1SqLength * n2SqLength;
+
+            pType aCoef = (secondD * nScalarProduct - firstD  * n2SqLength) / commonDenominator;
+            pType bCoef = (firstD  * nScalarProduct - secondD * n1SqLength) / commonDenominator;
+            
+            commonP = aCoef * firstNormalVec + bCoef * secondNormalVec;
+        
+        }
+        
+        template<typename pType>
+        void ProjectEdges  (pType projection [3], const Triangle<pType>& tr, 
+                            const Vector<pType>& leadVec, const Vector<pType>& commonP) {
+        
+            for (int i = 0; i < 3; i++) {
+            
+                projection [i] = (tr.getVec (i) - commonP) * leadVec;
+            
+            }
+        
+        }
+
+        template<typename pType>
+        pType CalcDist (const Vector<pType>& normalV, const pType dCoef, const Vector<pType>& point) {
+        
+            pType dist = std::abs  (normalV.getCoord (0) * point.getCoord (0) + 
+                                    normalV.getCoord (1) * point.getCoord (1) +
+                                    normalV.getCoord (2) * point.getCoord (2) + dCoef) / std::sqrt(normalV.squareLength ());
+
+            return dist; 
+        
+        }
+
+        template<typename pType>
+        void CalcTParams (pType tParams [2], const pType projection [3], 
+                          const Vector<pType> normalV, const pType dCoef, const Triangle<pType>& tr) {
+        
+            pType distThirdVert = CalcDist (normalV, dCoef, tr.getVec (2));
+            for (int i = 0; i < 2; i++) {
+                
+                pType verticeDist = CalcDist (normalV, dCoef, tr.getVec (i));
+                pType distFrac = verticeDist / (verticeDist - distThirdVert);
+
+                tParams [i] = projection [i] + (projection [2] - projection [i]) * distFrac;
+                std::cout << "here " << distThirdVert << std::endl;
+            
+            }
+        
+        }
+
+        template<typename pType>
+        bool Intersect3DTriangles (const Triangle<pType>& tr1, const Triangle<pType>& tr2) {
+            //Normal vector for the first plane
+            Vector<pType> firstNormalVec;
+            tr1.calcNormal (firstNormalVec);
+            
+            //Coef D for the first plane
+            pType firstD = 0;
+            tr1.calcCoefD (firstNormalVec, firstD);
+
+            //Normal vector for the second plane
+            Vector<pType> secondNormalVec;
+            tr2.calcNormal (secondNormalVec);
+            
+            //Coef D for the second plane
+            pType secondD = 0;
+            tr2.calcCoefD (secondNormalVec, secondD);
+
+            //leading vector for the common lane
+            Vector<pType> leadVec = firstNormalVec ^ secondNormalVec;
+            
+            Vector<pType> commonP;
+            CountCommonP (firstD, secondD, firstNormalVec, secondNormalVec, commonP);
+
+            //result: now we have common point and direction vector for the common lane
+            std::cout << "Common point: " << commonP << std::endl;
+            std::cout << "Direction: " << leadVec << std::endl;
+
+            //project triangle's vertices
+            pType firstProj [3] = {};
+            ProjectEdges (firstProj, tr1, leadVec, commonP);
+            std::cout << "proj " << firstProj [0] << " " << firstProj [1] << " " << firstProj [2] << std::endl;
+            pType secondProj [3] = {};
+            ProjectEdges (secondProj, tr2, leadVec, commonP);
+
+            //now let's compute t_{0,i} params. Better watch GCT 578 page 
+            pType firstTParams [2] = {};
+            CalcTParams (firstTParams, firstProj, firstNormalVec, firstD, tr1);
+            
+            pType secondTParams [2] = {};
+            CalcTParams (secondTParams, secondProj, secondNormalVec, secondD, tr2);
+
+            for (size_t i = 0; i < 2; i++) {
+            
+                std::cout << "firstT: "  << firstTParams  [i] << std::endl;
+                std::cout << "secondT: " << secondTParams [i] << std::endl;
+            
+            }
+
+            return 0;
         }
 
 }
--- triangles_together/main.cpp
+++ triangles_together/main.cpp
@@ -3,12 +3,20 @@
 
 int main()
 {
-	int countTriangles;
-	std::cin >> countTriangles;
+	//int countTriangles;
+	//std::cin >> countTriangles;
 
-	Tree::Octotree <double> tree;
+	//Tree::Octotree<double> tree;
 
-	tree.fillTree(countTriangles);
+	//tree.fillTree(countTriangles);
 
-	return 0;
+    GObjects::Triangle<double> tr1 ({0, 0, 0}, {0, 1, 0}, {1, 0, 0});
+    std::cout << tr1 << std::endl;
+
+    GObjects::Triangle<double> tr2 ({0, 0, 0}, {0, 1, 0}, {0, 0, 1});
+    std::cout << tr2 << std::endl;
+
+    bool test = GObjects::Intersect3DTriangles<double> (tr1, tr2);
+
+    return 0;
 }
