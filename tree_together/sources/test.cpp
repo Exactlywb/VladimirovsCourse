d3b5e327bda0bd3b9dcd14c92e1d4c7fcf3f09ce
PozhiloyPumba <ivanov.in@phystech.edu>
2021-11-07 03:59:06 +0300

    added some tests

--- tree_together/sources/test.cpp
+++ tree_together/sources/test.cpp
@@ -1,8 +1,21 @@
-#include "tree.hpp"
+#include <iostream>
 
-int main () {
+int main(int argc, char **argv) {
 
-    std::cout << "Test" << std::endl;
-    return 0;
+    bool *arr = new bool [10000] {};
+
+    for (int i = 0; i < 1000000; ++i) {
+        int rand = std::rand() % 10000;
+        if (arr[rand] == 0) {
+            arr[rand] = 1;
+            printf("k %d ", rand + 1);
+        }
+    }
+
+    for (int i = 0; i < 10000; ++i) {
+        if (arr[i] == 0) {
+            printf("k %d ", i + 1);
+        }
+    }
 
 }
