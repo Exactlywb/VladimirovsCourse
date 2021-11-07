49e94bba13980ee56b676eb46a571689a804d7ff
belov.vv@phystech.edu <belov.vv@phystech.edu>
2021-11-07 04:50:10 +0300

    Test README

--- tree_together/README.md
+++ tree_together/README.md
@@ -1,2 +1,32 @@
-# Tree
-(not yet)Implemented by Ivan Ivanov, WhatIsLove Belov, Frolov Daniil
+# TREE
+==================================================================
+# How to build 
+```
+  $ mkdir build
+  $ cd build
+  $ cmake ..
+  $ make
+```
+## Compiler settings:)
+```
+  $ g++ -Wall -Wextra -Wpedantic -Wsign-promo 
+    -Wsign-conversion -Wdouble-promotion
+    -Wfloat-equal -Wbool-compire -Wdiv-by-zero
+    -O2 
+```
+---
+---
+### In a folder 'build' an executiable file 'tree' and  test-file 'test_tree' will appear
+---
+## Steps of implementation:
+* Build a Red-Black tree with an addition filed named **'subtreeSize'** --- the size of subtree of the node
+* Input queries from the ***stdin***
+* Execute every query, remember it in a vector and print the   result to ***stdout***  
+---
+## Some words about Red-Black-Tree
+### A Red-Black Tree is a tree with the next invariant:
+* Every node is either <H style="background:#FFFFFF"> <span style="color:#FF0000">Red</span></H> or <H style="background:#FFFFFF"> <span style="color:#000000">Black</span></H>
+* Root is <H style="background:#FFFFFF"> <span style="color:#000000">Black</span></H>
+* If node is <H style="background:#FFFFFF"> <span style="color:#FF0000">Red</span></H> then its childs are <H style="background:#FFFFFF"> <span style="color:#000000">Black</span></H>
+* For each node all simple paths to leaves-descendants include the same amount of <H style="background:#FFFFFF"> <span style="color:#000000">Black</span></H> nodes
+### The example of Red-Black Tree: 
--- tree_together/sources/tree.cpp
+++ tree_together/sources/tree.cpp
@@ -128,67 +128,6 @@ namespace TreeImpl {
         }
 
     }
-
-    namespace {
-
-        void PrintNodes (std::ofstream& dumpOut, const Node* curNode) {
-            if (curNode == nullptr)
-                return;
-            
-            static int curNodeNum = 0;
-            int curInd = curNodeNum;
-            
-            if (curNode->color_ == BLACK) {
-                dumpOut << "\"box"  << curNodeNum << "\" [shape = \"record\", color = \"white\" label = <<font color = \"#242424\">" 
-                                    << curNode->val_ << ", sizeSubtree = " << curNode->subtreeSize << "</font>>]" << std::endl;
-            }
-            else if (curNode->color_ == RED) {
-                dumpOut << "\"box" << curNodeNum << "\" [shape = \"record\", color=\"white\", label = <<font color = \"#c2453c\">" 
-                        << curNode->val_ << ", sizeSubtree = " << curNode->subtreeSize << "</font>>]" << std::endl;
-            }
-            else {
-
-                std::cout << "Unexpected color in function " << __func__ << std::endl;
-                return; 
-
-            }
-
-            curNodeNum++;
-
-            if (curNode->left_ != nullptr) {
-
-                size_t nextIndex = curNodeNum;
-                PrintNodes (dumpOut, curNode->left_);
-                dumpOut << "\"box" << curInd << "\" -> \"box"<< nextIndex << "\"" << std::endl;
-
-            }
-
-            if (curNode->right_ != nullptr) {
-
-                size_t nextIndex = curNodeNum;
-                PrintNodes (dumpOut, curNode->right_);
-                dumpOut << "\"box" << curInd << "\" -> \"box"<< nextIndex << "\"" << std::endl;
-
-            }
-
-        }
-
-    }
-
-    //=====================================================================================================
-
-    void Tree::graphDump (const char* fileName) {
-
-        std::ofstream dumpOut (fileName, dumpOut.out | dumpOut.trunc);
-
-        dumpOut << "digraph Tree {\n";
-
-        PrintNodes (dumpOut, root);
-
-        dumpOut << "}";
-
-    }
-
     //-----------------------------------------------------------------------------------------------------
 
     void Tree::rightRotate (Node *x) {
@@ -397,5 +336,64 @@ namespace TreeImpl {
                 curNode = curNode->left_;
         }
         return amount;
+    }
+     namespace {
+
+        void PrintNodes (std::ofstream& dumpOut, const Node* curNode) {
+            if (curNode == nullptr)
+                return;
+            
+            static int curNodeNum = 0;
+            int curInd = curNodeNum;
+            
+            if (curNode->color_ == BLACK) {
+                dumpOut << "\"box"  << curNodeNum << "\" [shape = \"record\", color = \"white\" label = <<font color = \"#242424\">" 
+                                    << curNode->val_ << ", sizeSubtree = " << curNode->subtreeSize << "</font>>]" << std::endl;
+            }
+            else if (curNode->color_ == RED) {
+                dumpOut << "\"box" << curNodeNum << "\" [shape = \"record\", color=\"white\", label = <<font color = \"#c2453c\">" 
+                        << curNode->val_ << ", sizeSubtree = " << curNode->subtreeSize << "</font>>]" << std::endl;
+            }
+            else {
+
+                std::cout << "Unexpected color in function " << __func__ << std::endl;
+                return; 
+
+            }
+
+            curNodeNum++;
+
+            if (curNode->left_ != nullptr) {
+
+                size_t nextIndex = curNodeNum;
+                PrintNodes (dumpOut, curNode->left_);
+                dumpOut << "\"box" << curInd << "\" -> \"box"<< nextIndex << "\"" << std::endl;
+
+            }
+
+            if (curNode->right_ != nullptr) {
+
+                size_t nextIndex = curNodeNum;
+                PrintNodes (dumpOut, curNode->right_);
+                dumpOut << "\"box" << curInd << "\" -> \"box"<< nextIndex << "\"" << std::endl;
+
+            }
+
+        }
+
+    }
+
+    //=====================================================================================================
+
+    void Tree::graphDump (const char* fileName) {
+
+        std::ofstream dumpOut (fileName, dumpOut.out | dumpOut.trunc);
+
+        dumpOut << "digraph Tree {\n";
+
+        PrintNodes (dumpOut, root);
+
+        dumpOut << "}";
+
     }
 }
