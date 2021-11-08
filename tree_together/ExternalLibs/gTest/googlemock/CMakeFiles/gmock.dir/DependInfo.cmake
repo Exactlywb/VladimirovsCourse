2aa53de1480343571eeb24a01f9bbab0b3c24c99
ExactlyWb <frolov.da@phystech.edu>
2021-11-08 05:24:15 +0500

    This city needed a hero, so I've done google tests.

---
+++ tree_together/ExternalLibs/gTest/googlemock/CMakeFiles/gmock.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/exactlywb/googletest/googlemock/src/gmock-all.cc" "/home/exactlywb/Desktop/VladimirovsCourse/tree_together/ExternalLibs/gTest/googlemock/CMakeFiles/gmock.dir/src/gmock-all.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/exactlywb/googletest/googlemock/include"
+  "/home/exactlywb/googletest/googlemock"
+  "/home/exactlywb/googletest/googletest/include"
+  "/home/exactlywb/googletest/googletest"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/exactlywb/Desktop/VladimirovsCourse/tree_together/ExternalLibs/gTest/googletest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- tree_together/sources/test.cpp
+++ tree_together/sources/test.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <gtest/gtest.h>
 #include "tree.hpp"
 
 void CopyTest ();
@@ -6,17 +7,65 @@ void MoveTest ();
 void AssignmentTest ();
 void DumpAnswer (TreeImpl::Tree &firstTree, TreeImpl::Tree &secondTree,
                 const char* method, const char* treeToDump);
-void getRand (long long n);
+// void getRand (long long n);
 
-int main () {
+TEST( testTree, checkFirstFile ) {
+
+    system ("./tree < ../tests/1.txt > outTest1.txt");
+    
+    std::ifstream resFile   ("outTest1.txt");
+    std::string res         ((std::istreambuf_iterator<char>(resFile)),
+                              std::istreambuf_iterator<char>());
+
+    std::ifstream answFile  ("../tests/answ1.txt");
+    std::string answ        ((std::istreambuf_iterator<char>(answFile)),
+                              std::istreambuf_iterator<char>());
+
+    ASSERT_EQ(answ, res);
+
+}
+
+TEST( testTree, checkSecondFile ) {
+
+    system ("./tree < ../tests/2.txt > outTest2.txt");
+    
+    std::ifstream resFile   ("outTest2.txt");
+    std::string res         ((std::istreambuf_iterator<char>(resFile)),
+                              std::istreambuf_iterator<char>());
+
+    std::ifstream answFile  ("../tests/answ2.txt");
+    std::string answ        ((std::istreambuf_iterator<char>(answFile)),
+                              std::istreambuf_iterator<char>());
+
+    ASSERT_EQ(answ, res);
+
+}
+
+TEST( testTree, checkThirdFile ) {
+
+    system ("./tree < ../tests/3.txt > outTest3.txt");
+    
+    std::ifstream resFile   ("outTest3.txt");
+    std::string res         ((std::istreambuf_iterator<char>(resFile)),
+                              std::istreambuf_iterator<char>());
+
+    std::ifstream answFile  ("../tests/answ3.txt");
+    std::string answ        ((std::istreambuf_iterator<char>(answFile)),
+                              std::istreambuf_iterator<char>());
+
+    ASSERT_EQ(answ, res);
+
+}
+
+int main (int argc, char** argv) {
 
     // std::cout << "Test" << std::endl;
     // CopyTest ();
     // MoveTest ();
     // AssignmentTest ();
 
-    getRand (100000000);
-    return 0;
+    testing::InitGoogleTest (&argc, argv);
+    return RUN_ALL_TESTS ();
 
 }
 
@@ -94,23 +143,23 @@ void DumpAnswer (TreeImpl::Tree &firstTree, TreeImpl::Tree &secondTree,
     }
 }
 
-void getRand (long long n) {
+// void getRand (long long n) {
 
-    bool *arr = new bool [n] {};
+//     bool *arr = new bool [n] {};
 
-    for (int i = 0, end = n * 100; i < end; ++i) {
-        long long rand = std::rand() % n;
-        if (arr[rand] == 0) {
-            arr[rand] = 1;
-            printf("k %lld ", rand + 1);
-        }
-    }
+//     for (int i = 0, end = n * 100; i < end; ++i) {
+//         long long rand = std::rand() % n;
+//         if (arr[rand] == 0) {
+//             arr[rand] = 1;
+//             printf("k %lld ", rand + 1);
+//         }
+//     }
 
-    for (long long i = 0; i < n; ++i) {
-        if (arr[i] == 0) {
-            printf("k %lld ", i + 1);
-        }
-    }
+//     for (long long i = 0; i < n; ++i) {
+//         if (arr[i] == 0) {
+//             printf("k %lld ", i + 1);
+//         }
+//     }
 
-    delete [] arr;
-}
+//     delete [] arr;
+// }
