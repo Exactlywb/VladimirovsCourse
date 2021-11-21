7272e11a778f33e8ffe00b8b56233c4ce0f60ddf
belov.vv@phystech.edu <belov.vv@phystech.edu>
2021-11-21 16:32:49 +0300

    Fix CMake

--- VulkanTest/build/CMakeFiles/vulktut.dir/DependInfo.cmake
+++ VulkanTest/build/CMakeFiles/vulktut.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/kolobaka2000/Ilab_Together/VladimirovsCourse/VulkanTest/main.cpp" "/home/kolobaka2000/Ilab_Together/VladimirovsCourse/VulkanTest/build/CMakeFiles/vulkan.dir/main.cpp.o"
+  "/home/kolobaka2000/Ilab_Together/VladimirovsCourse/VulkanTest/main.cpp" "/home/kolobaka2000/Ilab_Together/VladimirovsCourse/VulkanTest/build/CMakeFiles/vulktut.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
