378e967e1f3602645d1a5ce1bb31ad665fc1048b
ExactlyWb <frolov.da@phystech.edu>
2021-11-21 22:39:08 +0500

    Added logical devices handling

--- VulkanTest/main.cpp
+++ VulkanTest/main.cpp
@@ -17,10 +17,13 @@ class HelloTriangleApplication {
     const uint32_t height   = 600;
 
     GLFWwindow* window;
+
     VkInstance  instance;
+    VkDevice    device;
+
+    VkQueue     graphicsQueue;
 
     VkDebugUtilsMessengerEXT debugMessenger;
-
     VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
 
     const std::vector<const char*> validationLayers = {"VK_LAYER_KHRONOS_validation"};
@@ -285,11 +288,53 @@ class HelloTriangleApplication {
         
     }
 
+    void createLogicalDevice () {
+
+        QueueFamilyIndices indices = findQueueFamilies (physicalDevice);
+
+        VkDeviceQueueCreateInfo queueCreateInfo {};
+
+        queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+        queueCreateInfo.queueFamilyIndex = indices.graphicsFamily.value ();
+        queueCreateInfo.queueCount = 1;
+
+        float queuePriority = 1.0f;
+        queueCreateInfo.pQueuePriorities = &queuePriority;
+
+        VkPhysicalDeviceFeatures deviceFeatures {};
+
+        VkDeviceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+
+        createInfo.pQueueCreateInfos = &queueCreateInfo;
+        createInfo.queueCreateInfoCount = 1;
+
+        createInfo.pEnabledFeatures = &deviceFeatures;
+
+        createInfo.enabledExtensionCount = 0;
+        if (enableValidationLayers) {
+
+            createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size ());
+            createInfo.ppEnabledLayerNames = validationLayers.data ();
+
+        } else
+            createInfo.enabledLayerCount = 0;
+
+        if (vkCreateDevice(physicalDevice, &createInfo, nullptr, &device) != VK_SUCCESS)
+            throw std::runtime_error("failed to create logical device!");
+
+        vkGetDeviceQueue(device, indices.graphicsFamily.value (), 0, &graphicsQueue);
+
+    }
+
     void initVulkan () {
 
         createInstance      ();
+
         setupDebugMessenger ();
-        pickPhysicalDevice  (instance);
+
+        pickPhysicalDevice  (instance); //!TODO maybe it's better not to transfer arguments
+        createLogicalDevice ();
 
     }
     
@@ -308,6 +353,7 @@ class HelloTriangleApplication {
         if (enableValidationLayers)
             DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
 
+        vkDestroyDevice     (device, nullptr);
         vkDestroyInstance   (instance, nullptr);
 
         glfwDestroyWindow   (window);
