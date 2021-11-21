34034b6cf5b3221b507876ea6f94b17a4976e000
ExactlyWb <frolov.da@phystech.edu>
2021-11-21 21:56:36 +0500

    Added physical devices

--- VulkanTest/main.cpp
+++ VulkanTest/main.cpp
@@ -5,6 +5,8 @@
 #include <cstdlib>
 #include <vector>
 #include <cstring>
+#include <map>
+#include <optional>
 
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
@@ -19,7 +21,19 @@ class HelloTriangleApplication {
 
     VkDebugUtilsMessengerEXT debugMessenger;
 
+    VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+
     const std::vector<const char*> validationLayers = {"VK_LAYER_KHRONOS_validation"};
+
+    struct QueueFamilyIndices {
+
+        std::optional<uint32_t> graphicsFamily;
+
+        bool isComplete () {
+            return graphicsFamily.has_value();
+        }
+
+    };
 
     #ifdef NDEBUG
         const bool enableValidationLayers = false;
@@ -139,10 +153,8 @@ class HelloTriangleApplication {
 
         }
 
-        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
             throw std::runtime_error("failed to create instance!");
-        }
-        
 
     }
 
@@ -181,10 +193,103 @@ class HelloTriangleApplication {
 
     }
 
+    bool isDeviceSuitable (VkPhysicalDevice device) {
+
+        VkPhysicalDeviceProperties deviceProperties;
+        VkPhysicalDeviceFeatures deviceFeatures;
+        vkGetPhysicalDeviceProperties(device, &deviceProperties);
+        vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
+
+        return  (deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU) &&
+                deviceFeatures.geometryShader;
+
+    }
+
+    void pickPhysicalDevice (VkInstance instance) {
+
+        uint32_t deviceCount = 0;
+        vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+        if (deviceCount == 0)
+            throw std::runtime_error("failed to find GPUs with Vulkan support!");
+
+        std::vector<VkPhysicalDevice> devices(deviceCount);
+        vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+
+        // Use an ordered map to automatically sort candidates by increasing score
+        std::multimap<int, VkPhysicalDevice> candidates;
+        for (const auto& device : devices) {
+
+            int score = rateDeviceSuitability(device);
+            if (score > 0)
+                candidates.insert(std::make_pair(score, device));
+
+        }
+
+        // Check if the best candidate is suitable at all
+        if (candidates.rbegin()->first > 0)
+            physicalDevice = candidates.rbegin()->second;
+        else
+            throw std::runtime_error("failed to find a suitable GPU!");
+
+    }
+
+    QueueFamilyIndices findQueueFamilies (VkPhysicalDevice device) {
+
+        QueueFamilyIndices indices;
+
+        uint32_t queueFamilyCount = 0;
+        vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+
+        std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+        vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+
+        int queueFamIndex = 0;
+        for (const auto& queueFamily : queueFamilies) {
+
+            if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT)
+                indices.graphicsFamily = queueFamIndex;
+
+            if (indices.isComplete())
+                break;
+
+            ++queueFamIndex;
+
+        }
+
+        return indices;
+
+    }
+
+    int rateDeviceSuitability(VkPhysicalDevice device) {
+
+        if (!findQueueFamilies (device).isComplete()) return -1;
+
+        VkPhysicalDeviceProperties deviceProperties;
+        VkPhysicalDeviceFeatures deviceFeatures;
+        vkGetPhysicalDeviceProperties(device, &deviceProperties);
+        vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
+        int score = 0;
+
+        //Discrete GPUs have a significant performance advantage
+        if (deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU)
+            score += 1000;
+
+        // Maximum possible size of textures affects graphics quality
+        score += deviceProperties.limits.maxImageDimension2D;
+
+        // Application can't function without geometry shaders
+        if (!deviceFeatures.geometryShader)
+            return 0;
+
+        return score;
+        
+    }
+
     void initVulkan () {
 
         createInstance      ();
         setupDebugMessenger ();
+        pickPhysicalDevice  (instance);
 
     }
     
