08ae20f444a954908f4f7a45abc2491bcd4bd73f
pozhiloyPumba <ivanov.in@phystech.edu>
2021-11-24 01:08:06 +0300

    remove unused directory

--- VulkanTest/vulkan/shaders/compile.sh
+++
@@ -1,3 +0,0 @@
-cwd=$(pwd)
-/usr/local/bin/glslc ${cwd}/../vulkan/shaders/shader.vert -o ${cwd}/../vulkan/shaders/vert.spv
-/usr/local/bin/glslc ${cwd}/../vulkan/shaders/shader.frag -o ${cwd}/../vulkan/shaders/frag.spv
