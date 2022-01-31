771662d91b84acd289b632e84dcb038c779d6585
belov.vv@phystech.edu <belov.vv@phystech.edu>
2022-01-31 23:51:12 +0300

    add find and end

--- tree_together/lvl_2/sources/main.cpp
+++ tree_together/lvl_2/sources/main.cpp
@@ -3,13 +3,19 @@
 int main ()
 {
     TreeImpl::SplayTree<int> tree;
+    
+    
 
-    for (int i = 0; i < 100000; ++i) {
-        int x;
-        std::cin >> x;
-        tree.insert (x);
+    for (int i = 0; i < 100; ++i) {
+        // int x;
+        // std::cin >> x;
+        tree.insert (i);
     }
     std::cout << tree.begin ()->val_ << std::endl;
+
+    std::cout << *(tree.find(123)) << std::endl;
+
+
 
     tree.graphDump ("out.dot");
 
