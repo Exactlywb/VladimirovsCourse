a44fa0b30d047beed64e7f09782332501520f331
ExactlyWb <frolov.da@phystech.edu>
2022-02-18 01:58:13 +0500

    Added structs to evaluate and apply

--- ParaCL/backend/interpreter/interpreter.hpp
+++ ParaCL/backend/interpreter/interpreter.hpp
@@ -40,7 +40,7 @@ namespace interpret {
 
     };
 
-    struct FuncObjScope final: public ScopeWrapper { //!TODO
+    struct FuncObjScope final: public ScopeWrapper {
         class FuncComp {
 
             AST::FuncNode* funcDecl_ = nullptr;
@@ -98,7 +98,7 @@ namespace interpret {
         
             for (auto&& obj = tbl_.cbegin (), end = tbl_.cend (); obj != end; ++obj) {
 
-                if ((*obj)->getName () == name) //Note! The same name is about global var
+                if ((*obj)->getName () == name)
                     return obj;
 
             }
@@ -141,6 +141,97 @@ namespace interpret {
         void run ();
 
     }; 
+
+    class EvalApplyNode {
+
+        AST::Node* node_;
+        EvalApplyNode* parent_ = nullptr;
+
+    public:
+        EvalApplyNode (AST::Node* node): node_ (node) {}
+
+        AST::Node* getNode () const { return node_; }
+        virtual const EvalApplyNode *eval (Context& context) const = 0;
+
+    };
+
+    class EAScope final: public EvalApplyNode {
+
+    public:
+        EAScope (AST::ScopeNode* astScope): EvalApplyNode (astScope) {}
+        const EvalApplyNode *eval (Context& context) const override; //!TODO
+
+    };
+
+    class EAWhile final: public EvalApplyNode {
+
+    public:
+        EAWhile (AST::CondNode* astCond): EvalApplyNode (astCond) {}
+        const EvalApplyNode *eval (Context& context) const override; //!TODO
+
+    };
+
+    class EAIf final: public EvalApplyNode {
+
+    public:
+        EAIf (AST::CondNode* astCond): EvalApplyNode (astCond) {}
+        const EvalApplyNode *eval (Context& context) const override; //!TODO
+
+    };
+
+    template <typename operT>
+    class EABinOp final: public EvalApplyNode {
+
+    public:
+        EABinOp (AST::OperNode* astOper): EvalApplyNode (astOper) {}
+        const EvalApplyNode *eval (Context& context) const override; //!TODO
+
+    };
+
+    template <typename operT>
+    class EAUnaryOp final: public EvalApplyNode {
+
+    public:
+        EAUnaryOp (AST::OperNode* astOper): EvalApplyNode (astOper) {}
+        const EvalApplyNode *eval (Context& context) const override; //!TODO
+
+    };
+
+    class EAReturn final: public EvalApplyNode {
+
+    public:
+        EAReturn (AST::OperNode* astOper): EvalApplyNode (astOper) {}
+        const EvalApplyNode *eval (Context& context) const override; //!TODO
+
+    };
+
+    class EAAssign final: public EvalApplyNode {
+
+    public:
+        EAAssign (AST::OperNode* astOper): EvalApplyNode (astOper) {}
+        const EvalApplyNode *eval (Context& context) const override; //!TODO
+
+    };
+
+    struct BinOpAdd {
+
+        int operator() (const VarScope& lhs, const VarScope& rhs) const {
+
+            //!TODO
+
+        }
+
+    };
+
+    struct UnaryOpPrint {
+
+        int operator() (const VarScope& val) {
+
+            //!TODO
+
+        }
+
+    };
 
 }  // namespace interpret
 
